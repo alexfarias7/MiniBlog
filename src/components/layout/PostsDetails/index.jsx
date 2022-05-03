e9612f499572cc739e0930605960a5fcc27af88c
alexfarias7 <alexfarias44@gmail.com>
2022-05-03 00:25:32 -0300

    css in posts details

--- src/components/layout/PostsDetails/index.jsx
+++ src/components/layout/PostsDetails/index.jsx
@@ -4,21 +4,21 @@ import * as Styled from './style'
 
 function PostDetails({post}) {
     return ( 
-        <div>
+        <Styled.PostDetails>
             <img src={post.image} alt={post.title} />
             <h2>{post.title}</h2>
 
-            <p>{post.createdBy}</p>
+            <Styled.CreatedBy>{post.createdBy}</Styled.CreatedBy>
 
-            <div>
+            <Styled.Tags className='tags'>
                 {post.tagsArray.map((tag)=>(
                     <p key={tag}> <span>#</span> {tag}</p>
                 ))}
-            </div>
+            </Styled.Tags>
             <Styled.BtnOutline to={`/posts/${post.id}`} >
                 Ler
             </Styled.BtnOutline>
-        </div>
+        </Styled.PostDetails>
 
      );
 }
--- src/components/layout/PostsDetails/style.js
+++ src/components/layout/PostsDetails/style.js
@@ -3,7 +3,47 @@ import styled from "styled-components";
 import {Link} from 'react-router-dom'
 
 export const BtnOutline = styled(Link)`
+ background-color: transparent;
+  border-radius: 0;
+  color: #000;
+  border: 1px solid #000;
+  padding: 7px 30px;
+ 
  &:hover {
     background-color: #dc3545;
   }
+`
+
+export const PostDetails = styled.div`
+margin-bottom: 2em;
+
+& img {
+    max-width: 600px;
+}
+
+& h2 {
+    margin-bottom: 0.4em;
+}
+
+& .tags{
+    margin-bottom:1.2em;
+    display: flex;
+}
+`
+
+export const Tags = styled.div`
+
+& p{
+    margin-right: .5em;
+}
+
+& span{
+    font-weight: bold;
+}
+`
+export const CreatedBy = styled.div`
+font-style: italic;
+color: #444;
+font-size: 0.8;
+margin-bottom: 1em;
 `
